bbbd232a6494cbb757c74273eb79312508fcb798
JIHYE <108344293+JGJBDHK6@users.noreply.github.com>
2022-09-13 21:16:10 +0900

    Delete JIHYE directory

--- JIHYE/CodeUp6061-6070/6064_28033048(AC).py
+++
@@ -1,8 +0,0 @@
-﻿a, b, c = input().split()
-a = int(a)
-b = int(b)
-c = int(c)
-
-v = (a if a < b else b) if ((a if a < b else b) < c) else c
-
-print(v)
--- JIHYE/codeup/6061-6070/6061_28032968(AC).py
+++
@@ -1,6 +0,0 @@
-﻿a, b = input().split()
-a = int(a)
-b = int(b)
-c = a | b
-
-print(c)
